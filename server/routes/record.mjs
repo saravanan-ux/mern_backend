a7376bce109e187037e035e05a63df4db48f08e3
Dineshchand <dineshchandgr@gmail.com>
2024-02-14 02:49:04 +0800

    Update record.mjs

--- server/routes/record.mjs
+++ server/routes/record.mjs
@@ -36,13 +36,27 @@ router.post("/", async (req, res) => {
 // This section will help you update a record by id.
 router.patch("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
+  const updates =  {
+    $set: {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level
+    }
+  };
 
+  let collection = await db.collection("records");
+  let result = await collection.updateOne(query, updates);
+  res.send(result).status(200);
 });
 
 // This section will help you delete a record
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
+  const collection = db.collection("records");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
 });
 
 export default router;
